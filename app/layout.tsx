f809326e484d19996d56cb582ab9377b71cffd48
Jonathan <jwmp001@gmail.com>
2023-07-18 12:16:25 -0400

    Added keywords in metadata and changed global.css import to global.scss.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import './globals.scss'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Jonathan W. Mendez',
   description: 'The personal website of Jonathan Mendez',
+  keywords: 'Jonathan Mendez, Jonathan W. Mendez, Jonathan, Mendez, W. Mendez, html, css, javascript, typescript, react, next.js, web development, web design, software, engineer, software engineer, developer, professional, professional musician, music, musician, brass, low brass, trombone, tenor trombone, bass trombone, tuba, euphonium, composer, arranger, educator, teacher, private lessons, private music lessons, music lessons,',
 }
 
 export default function RootLayout({
