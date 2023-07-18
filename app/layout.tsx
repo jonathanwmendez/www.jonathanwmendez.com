6ea3fef88dda58783efc253e1250f0f6dcb7827d
Jonathan <jwmp001@gmail.com>
2023-07-18 12:49:03 -0400

    Added more keywords and a main tag around the children.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Jonathan W. Mendez',
   description: 'The personal website of Jonathan Mendez',
-  keywords: 'Jonathan Mendez, Jonathan W. Mendez, Jonathan, Mendez, W. Mendez, html, css, javascript, typescript, react, next.js, web development, web design, software, engineer, software engineer, developer, professional, professional musician, music, musician, brass, low brass, trombone, tenor trombone, bass trombone, tuba, euphonium, composer, arranger, educator, teacher, private lessons, private music lessons, music lessons,',
+  keywords: 'Jonathan Mendez, Jonathan W. Mendez, Jonathan, Mendez, W. Mendez, html, css, javascript, typescript, react, next.js, web development, web design, software, engineer, software engineer, developer, professional, professional musician, music, musician, brass, low brass, trombone, tenor trombone, bass trombone, tuba, euphonium, composer, arranger, educator, teacher, private lessons, private music lessons, music lessons, bootstrap,',
 }
 
 export default function RootLayout({
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
